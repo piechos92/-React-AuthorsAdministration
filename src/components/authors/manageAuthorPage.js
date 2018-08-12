d6a6e33ed86a503c679c809ffd5de21f824555bb
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2018-08-12 23:09:51 +0200

    Add flux solution for unidirectional data flow

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -3,7 +3,8 @@
 var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
-var AuthorAPI = require('../../api/authorApi');
+var AuthorActions = require('../../actions/authorActions');
+var AuthorStore = require('../../stores/authorStore');
 var Toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
@@ -28,11 +29,11 @@ var ManageAuthorPage = React.createClass({
         };
     },
 
-    componentWillMount: function() {
+    componentWillMount: function() { //componentWillMount instead of componentDidMount because this way rendering won't fire twice
         var authorId = this.props.params.id; //from the path 'author/:id'
 
         if(authorId) {
-            this.setState({author: AuthorAPI.getAuthorById(authorId)});
+            this.setState({author: AuthorStore.getAuthorById(authorId)});
         }
     },
 
@@ -74,7 +75,11 @@ var ManageAuthorPage = React.createClass({
             return;
         }
 
-        AuthorAPI.saveAuthor(this.state.author);
+        if(this.state.author.id){
+            AuthorActions.updateAuthor(this.state.author);
+        } else {
+            AuthorActions.createAuthor(this.state.author);
+        }
         this.setState({dirty: false});
         Toastr.success('Author saved');
         this.transitionTo('authors');
